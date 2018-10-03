79057012d4ba40d2d6606b1991db74939d18e5bd
zhougit86 <zhou16386@163.com>
2018-10-03 12:14:39 +0800

    temp save

---
+++ stream.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/conf.h>
+#include <stropts.h>
+
+using namespace std;
+int main(int argc,char *argv[]){
+    int fd,i,nmods;
+    struct str_list list;
+
+    fd = open(argv[i],O_RDONLY);
+    nmods = ioctl(fd, I_LIST, (void *)0);
+    cout << "# of modules:" << nmods << endl;
+
+    list.sl_modlist = calloc(nmods,sizeof(struct str_mlist));
+    list.sl_nmods = nmods;
+
+    for (i = 1; i <= nmods ; i++){
+        cout << list.sl_modlist++ << endl;
+    }
+
+}
