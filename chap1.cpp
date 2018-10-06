7312dc62b1e6e210816fc0bcaba4ad4452867b6a
zhougit86 <zhou16386@163.com>
2018-10-06 18:03:35 +0800

    char1

---
+++ chap1.cpp
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    DIR *dp;
+    dirent *dirp;
+
+    if ((dp = opendir(argv[1])) == NULL){
+        cout << "can not open" <<endl;
+    }
+    while ((dirp = readdir(dp))!=NULL){
+        cout << dirp->d_name;
+    }
+    closedir(dp);
+    exit(0);
+}
