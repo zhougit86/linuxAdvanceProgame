65ada030fcad0417832e99cd0b20990398e81aee
zhougit86 <zhou16386@163.com>
2018-11-14 21:56:00 +0800

    s

--- osX/intro.cpp
+++ osX/intro.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <unistd.h>
 
 using namespace std;
 
-int main(int argc, char* argv[]){
+int smain(int argc, char* argv[]){
     char *str = argv[1];
     while(1){
-        Spin(1);
+        sleep(1000);
         cout <<str<<endl;
     }
     return 0;
