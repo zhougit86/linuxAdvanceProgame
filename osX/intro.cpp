9e42db61bedde34666433896b38dd87ddc9c0baa
zhougit86 <zhou16386@163.com>
2018-11-14 22:15:28 +0800

    save

--- osX/intro.cpp
+++ osX/intro.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
 #include <unistd.h>
 
 using namespace std;
 
-int smain(int argc, char* argv[]){
-    char *str = argv[1];
-    while(1){
-        sleep(1000);
-        cout <<str<<endl;
-    }
+int main(int argc, char* argv[]){
+    // char *str = argv[1];
+    // while(1){
+    //     sleep(1);
+    //     cout <<str<<endl;
+    // }
+
+    int* p = malloc(sizeof(int));
+    printf("%p\n",p);
+
     return 0;
 }
