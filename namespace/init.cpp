fee7fb473c9256b2809c04407ef3b306937db3f0
zhougit86 <zhou16386@163.com>
2018-11-24 21:03:49 +0800

    first

---
+++ namespace/init.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <sched.h>
+
+using namespace std;
+
+int main(int argc , char* argv[]){
+    cout << "hello" <<endl;
+}
