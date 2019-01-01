94e3fc395cda403d84e4f1e21249beda10f8a734
zhougit86 <zhou16386@163.com>
2019-01-01 22:24:25 +0800

    haha

--- epoll/epoll_server.cpp
+++ epoll/epoll_server.cpp
@@ -3,6 +3,27 @@
 
 using namespace std;
 
+void addfd(int epollfd, int fd, int enable_et)
+{
+        struct epoll_event event;
+        // EPOLLIN :表示对应的文件描述符可以读(包括对端SOCKET正常关闭)
+        // POLLOUT:表示对应的文件描述符可以写
+        // EPOLLPRI:表示对应的文件描述符有紧急的数据可读(这里应该表示有带外数据到来)
+        // EPOLLERR:表示对应的文件描述符发生错误
+        // EPOLLHUP:表示对应的文件描述符被挂断;
+        // EPOLLET: 将EPOLL设为边缘触发(Edge Triggered)模式,这是相对于水平触发(Level Triggered)来说的
+        // EPOLLONESHOT:只监听一次事件,当监听完这次事件之后,如果还需要继续监听这个socket的话,需要再次把这个socket加入到EPOLL队列里
+        event.data.fd = fd;
+        event.events = EPOLLIN;
+        if(enable_et){
+                //边缘触发
+                event.events |= EPOLLET;
+        }
+        //第二个参数可以是EPOLL_CTL_MOD，EPOLL_CTL_DEL
+        epoll_ctl(epollfd, EPOLL_CTL_ADD, fd, &event);
+//      setnonblocking(fd);
+}
+
 int main(int argc, char* argv[]){
     cout<<"haha"<<endl;
 
