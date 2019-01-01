6969e583a0a772ba72363d7c55b64c41cd6af16a
zhougit86 <zhou16386@163.com>
2019-01-01 21:53:15 +0800

    temp save

--- epoll/epoll_server.cpp
+++ epoll/epoll_server.cpp
@@ -6,5 +6,11 @@ using namespace std;
 int main(int argc, char* argv[]){
     cout<<"haha"<<endl;
 
+
+    int epollfd = epoll_create(5);
+    cout<<epollfd<<endl;
+    if(epollfd < 0){
+        perr_exit("epoll_create err");
+    }
     return 0;
 }
