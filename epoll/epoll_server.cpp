f0e7165458f03f67e9451f8ecb1327fbeb2efa23
zhougit86 <zhou16386@163.com>
2019-01-02 22:27:01 +0800

    save

--- epoll/epoll_server.cpp
+++ epoll/epoll_server.cpp
@@ -70,7 +70,8 @@ void et(struct epoll_event *event, int num, int epollfd, int listenfd)
             int clilen = sizeof(clientaddr);
             int connfd = accept(listenfd, (struct sockaddr *)&clientaddr, (socklen_t *)&clilen);
             
-            cout << "accept complete:" << event[i].events & EPOLLIN  <<endl;
+            cout << "accept complete:"  <<endl;
+            printf("%d\n",event[i].events & EPOLLIN);
             
             // addfd(epollfd, connfd, true);//多connfd开启ET模式
         }else if(event[i].events & EPOLLIN){
