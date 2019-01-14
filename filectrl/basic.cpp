a5e15d82eeb113fb0e40c61a5624f64653d93531
zhougit86 <zhou16386@163.com>
2019-01-14 23:34:08 +0800

    fix

--- filectrl/basic.cpp
+++ filectrl/basic.cpp
@@ -13,6 +13,10 @@ int main(int argc , char* argv[]){
     f_attrib = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;  
     int fd = open("./haha",O_RDWR | O_CREAT | O_TRUNC, f_attrib );
 
+    write(fd,"333",3);
+    write(fd,"22",2);
+    write(fd,"1",1);
+
     close(fd);
     return 0;
 }
