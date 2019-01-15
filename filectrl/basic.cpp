dbb4475917e1f358c45631fe661ab3d093712af7
zhougit86 <zhou16386@163.com>
2019-01-15 21:24:04 +0800

    save user info

--- filectrl/basic.cpp
+++ filectrl/basic.cpp
@@ -4,7 +4,7 @@
 #include <sys/types.h>
 // 读写权限
 #include <sys/stat.h>
-// 关闭文件
+// 关闭文件,lseek都在这个库当中
 #include <unistd.h>
 
 int main(int argc , char* argv[]){
@@ -17,6 +17,22 @@ int main(int argc , char* argv[]){
     write(fd,"22",2);
     write(fd,"1",1);
 
+    //成功
+    printf("%d\n", lseek(fd,0,SEEK_CUR));
+    //成功
+    printf("%d\n", lseek(fd,1,SEEK_CUR));
+    //失败
+    printf("%d\n", lseek(fd,-21,SEEK_SET));
+
+
+    struct stat st;
+    fstat(fd, &st);
+
+    printf("size: %d\n",st.st_size);
+    printf("user: %d\n",st.st_uid);
     close(fd);
+
+    
+
     return 0;
 }
