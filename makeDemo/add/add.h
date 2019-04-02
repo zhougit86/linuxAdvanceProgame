a764cd7ddab66e4ddfac7a997dd20ca8164fe3b2
zhouxiaohu <80839961@yonghui.com>
2019-04-02 12:51:19 +0800

    fix make file

--- makeDemo/add/add.h
+++ makeDemo/add/add.h
@@ -1,4 +1,4 @@
-#ifdef __ADD_H__
+#ifndef __ADD_H__
 #define __ADD_H__
 
 extern int add (int a, int b);
--- makeDemo/sub/sub.h
+++ makeDemo/sub/sub.h
@@ -1,4 +1,4 @@
-#ifdef __SUB_H__
+#ifndef __SUB_H__
 #define __SUB_H__
 
 extern int sub (int a, int b);
